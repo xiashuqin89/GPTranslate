dd24378a364419ccaf7a1d61aba19f81d3b614d9
neolei <neolei@tencent.com>
2023-07-03 14:13:50 +0800

    first blood 4.1.1

--- log.py
+++ log.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 
-logger = logging.getLogger('GPTranslate')
+logger = logging.getLogger('root')
 default_handler = logging.StreamHandler(sys.stdout)
 default_handler.setFormatter(logging.Formatter(
     '[%(asctime)s %(name)s] %(levelname)s: %(message)s'
